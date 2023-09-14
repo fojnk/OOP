8e8aeeccf025750ddaae5e18c99cec065dee64c5
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-09-14 22:06:33 +0700

    format Main.java

--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -3,15 +3,15 @@ package org.example;
 
 public class Main {
 
-    private static void siftDown(int[] number_array, int root, int len){
+    private static void siftDown(int[] number_array, int root, int len) {
         int maxElem = root;
         int leftChild = root * 2 + 1;
         int rightChild = root * 2 + 2;
 
-        if (leftChild < len && number_array[leftChild] > number_array[root]){
+        if (leftChild < len && number_array[leftChild] > number_array[root]) {
             maxElem = leftChild;
         }
-        if (rightChild < len && number_array[rightChild] > number_array[maxElem]){
+        if (rightChild < len && number_array[rightChild] > number_array[maxElem]) {
             maxElem = rightChild;
         }
 
@@ -23,14 +23,14 @@ public class Main {
         }
     }
 
-    public static int[] heapsort(int[] number_array){
+    public static int[] heapsort(int[] number_array) {
         int n = number_array.length;
 
-        for (int i = n/2 - 1; i >= 0; i --) {
+        for (int i = n / 2 - 1; i >= 0; i--) {
             siftDown(number_array, i, n);
         }
 
-        for (int i = n - 1; i >=0 ; i --) {
+        for (int i = n - 1; i >= 0; i--) {
             int tmp = number_array[i];
             number_array[i] = number_array[0];
             number_array[0] = tmp;
@@ -39,6 +39,6 @@ public class Main {
         return number_array;
     }
 
-    public static void main(String[] args){}
+    public static void main(String[] args) {}
 
 }
