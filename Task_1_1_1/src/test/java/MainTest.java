ea98097c46427a48c9d98f1e923ce07d19959187
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-09-14 22:16:39 +0700

    more tests

--- Task_1_1_1/src/test/java/MainTest.java
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -7,6 +7,8 @@ public class MainTest {
     public void heapsortTest() {
         Assertions.assertArrayEquals(new int[] {1, 2, 3},Main.heapsort(new int[] {3, 1, 2}));
         Assertions.assertArrayEquals(new int[] {}, Main.heapsort(new int[] {}));
+        Assertions.assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7}, Main.heapsort(new int[] {2, 1, 6, 4, 5, 3, 7}));
+        Assertions.assertArrayEquals(new int[] {-3, -2, -1}, Main.heapsort(new int[] {-1, -2, -3}));
     }
 
 }
