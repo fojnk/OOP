761911f76479044bd6adb6850179ed3b4ee912ef
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-09-14 21:10:14 +0700

    add branch

---
+++ Task_1_1_1/src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class MainTest {
+    @Test
+    public void heapsortTest() {
+        Assertions.assertArrayEquals(new int[] {1, 2, 3},Main.heapsort(new int[] {3, 1, 2}));
+    }
+}
