d3697092fd297475b79649de26f4853ebe0e771a
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-09-14 20:47:42 +0700

    test jacoco

--- Task_1_1_1/build.gradle.kts
+++ Task_1_1_1/build.gradle.kts
@@ -20,5 +20,9 @@ tasks.test {
 }
 
 tasks.jacocoTestReport {
-    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
 }
