90f6e72da682601dc75b24a8560548c8220e84be
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-09-16 23:43:18 +0700

    fix codestyle-___-

--- Task_1_1_1/src/main/java/org/example/Heapsort.java
+++ Task_1_1_1/src/main/java/org/example/Heapsort.java
@@ -1,50 +1,51 @@
 package org.example;
+
 import java.util.Arrays;
 
 public class Heapsort {
 
-    private static void siftDown(int[] number_array, int root, int len) {
+    private static void siftDown(int[] numberArray, int root, int len) {
         int maxElem = root;
         int leftChild = root * 2 + 1;
         int rightChild = root * 2 + 2;
 
-        if (leftChild < len && number_array[leftChild] > number_array[root]) {
+        if (leftChild < len && numberArray[leftChild] > numberArray[root]) {
             maxElem = leftChild;
         }
-        if (rightChild < len && number_array[rightChild] > number_array[maxElem]) {
+        if (rightChild < len && numberArray[rightChild] > numberArray[maxElem]) {
             maxElem = rightChild;
         }
 
         if (maxElem != root) {
-            int tmp = number_array[root];
-            number_array[root] = number_array[maxElem];
-            number_array[maxElem] = tmp;
-            siftDown(number_array, maxElem, len);
+            int tmp = numberArray[root];
+            numberArray[root] = numberArray[maxElem];
+            numberArray[maxElem] = tmp;
+            siftDown(numberArray, maxElem, len);
         }
     }
 
-    public static int[] heapsort(int[] input_array) {
-        int[] number_array = input_array.clone();
-        int n = number_array.length;
+    public static int[] heapsort(int[] inputArray) {
+        int[] numberArray = inputArray.clone();
+        int n = numberArray.length;
 
         for (int i = n / 2 - 1; i >= 0; i--) {
-            siftDown(number_array, i, n);
+            siftDown(numberArray, i, n);
         }
 
         for (int i = n - 1; i >= 0; i--) {
-            int tmp = number_array[i];
-            number_array[i] = number_array[0];
-            number_array[0] = tmp;
-            siftDown(number_array, 0, i);
+            int tmp = numberArray[i];
+            numberArray[i] = numberArray[0];
+            numberArray[0] = tmp;
+            siftDown(numberArray, 0, i);
         }
-        return number_array;
+        return numberArray;
     }
 
-    public static void main(String[] Args) {
-        int[] input_array = new int[] {2, 3, 1, 5, 3, 4, 1};
-        int[] result_array = Heapsort.heapsort(input_array);
-        System.out.println(Arrays.toString(input_array));
-        System.out.println(Arrays.toString(result_array));
+    public static void main(String[] args) {
+        int[] inputArray = new int[] {2, 3, 1, 5, 3, 4, 1};
+        int[] resultArray = Heapsort.heapsort(inputArray);
+        System.out.println(Arrays.toString(inputArray));
+        System.out.println(Arrays.toString(resultArray));
     }
 
 }
