417102f5a7a69be863f5c633d66ec3dfa1fd81a3
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-09-21 13:49:00 +0700

    change PolynomialTest

--- Task_1_1_2/src/main/java/org/example/Main.java
+++ Task_1_1_2/src/main/java/org/example/Main.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import org.example.Polynomial;
 
 public class Main {
--- Task_1_1_2/src/test/java/org/example/PolynomialTest.java
+++ Task_1_1_2/src/test/java/org/example/PolynomialTest.java
@@ -11,49 +11,49 @@ public class PolynomialTest {
     @ParameterizedTest
     @MethodSource("generateDataForMinus")
     public void minusTest(Polynomial expected, Polynomial p1, Polynomial p2) {
-        Arguments.assertArrayEquals(expected.getCofArray(), p1.minus(p2).getCofArray());
+        Assertions.assertArrayEquals(expected.getCofArray(), p1.minus(p2).getCofArray());
     }
 
     @ParameterizedTest
     @MethodSource("generateDataForPlus")
     public void plusTest(Polynomial expected, Polynomial p1, Polynomial p2) {
-        Arguments.assertArrayEquals(expected.getCofArray(), p1.plus(p2).getCofArray());
+        Assertions.assertArrayEquals(expected.getCofArray(), p1.plus(p2).getCofArray());
     }
 
     @ParameterizedTest
     @MethodSource("generateDataForTimes")
     public void timesTest(Polynomial expected, Polynomial p1, Polynomial p2) {
-        Arguments.assertArrayEquals(expected.getCofArray(), p1.times(p2).getCofArray());
+        Assertions.assertArrayEquals(expected.getCofArray(), p1.times(p2).getCofArray());
     }
 
     @ParameterizedTest
-    @MethodSource("generateData")
-    public void evaluateTest(int[] expected, int[] input) {
-
+    @MethodSource("generateDataForEvaluate")
+    public void evaluateTest(int expected, Polynomial p, int value) {
+        Assertions.assertEquals(expected, p.evaluate(value));
     }
 
     @ParameterizedTest
-    @MethodSource("generateData")
-    public void differentiateTest(int[] expected, int[] input) {
-
+    @MethodSource("generateDataForDiff")
+    public void differentiateTest(Polynomial expected, Polynomial p1) {
+        Assertions.assertArrayEquals(expected.getCofArray(), p1.differentiate(1).getCofArray());
     }
 
     @ParameterizedTest
-    @MethodSource("generateData")
-    public void toStringTest(int[] expected, int[] input) {
-
+    @MethodSource("generateDataForStrings")
+    public void toStringTest(String expected, Polynomial p) {
+        Assertions.assertEquals(expected, p.toString());
     }
 
     @ParameterizedTest
-    @MethodSource("generateData")
-    public void equalsTest(int[] expected, int[] input) {
-
+    @MethodSource("generateDataForEquals")
+    public void equalsTest(boolean expected, Polynomial p1, Polynomial p2) {
+        Assertions.assertEquals(expected, p1.equals(p2));
     }
 
     static Stream<Arguments> generateDataForPlus() {
         return Stream.of(
                 Arguments.arguments(new Polynomial(new int[] {1, 2, 3, 4}), new Polynomial(new int[] {1, 2, 3, 3}), new Polynomial(new int[] {0, 0, 0, 1})),
-                Arguments.arguments(new Polynomial(new int[] {1, 2, 3}), new Polynomial(new int[] {1, 2, 3, 3}), new Polynomial(new int[] {1, 2, 3, -3})),
+                Arguments.arguments(new Polynomial(new int[] {1, 2, 3}), new Polynomial(new int[] {1, 2, 3, 3}), new Polynomial(new int[] {0, 0, 0, -3})),
                 Arguments.arguments(new Polynomial(new int[] {}), new Polynomial(new int[] {-1, -2, -3, -4}), new Polynomial(new int[] {1, 2, 3, 4}))
         );
     }
@@ -72,5 +72,37 @@ public class PolynomialTest {
                 Arguments.arguments(new Polynomial(new int[] {0, 0, 0, 6}), new Polynomial(new int[] {0, 2}), new Polynomial(new int[] {0, 0, 3})),
                 Arguments.arguments(new Polynomial(new int[] {1, 1, 1, 1}), new Polynomial(new int[] {1}), new Polynomial(new int[] {1, 1, 1, 1}))
                 );
+    }
+
+    static Stream<Arguments> generateDataForDiff() {
+        return Stream.of(
+                Arguments.arguments(new Polynomial(new int[] {2, 3}), new Polynomial(new int[] {1, 2, 3})),
+                Arguments.arguments(new Polynomial(new int[] {3}), new Polynomial(new int[] {2, 3})),
+                Arguments.arguments(new Polynomial(new int[] {}), new Polynomial(new int[] {3}))
+        );
+    }
+
+    static Stream<Arguments> generateDataForEvaluate() {
+        return Stream.of(
+                Arguments.arguments(6, new Polynomial(new int[] {1, 2, 3}), 1),
+                Arguments.arguments(0, new Polynomial(new int[] {}), 1),
+                Arguments.arguments(142, new Polynomial(new int[] {1, 2, 3, 4}), 3)
+        );
+    }
+
+    static Stream<Arguments> generateDataForEquals() {
+        return Stream.of(
+                Arguments.arguments(false, new Polynomial(new int[] {2, 3}), new Polynomial(new int[] {1, 2, 3})),
+                Arguments.arguments(true, new Polynomial(new int[] {2, 3}), new Polynomial(new int[] {2, 3})),
+                Arguments.arguments(false, new Polynomial(new int[] {}), new Polynomial(new int[] {3}))
+        );
+    }
+
+    static Stream<Arguments> generateDataForStrings() {
+        return Stream.of(
+                Arguments.arguments("2 + 3x", new Polynomial(new int[] {2, 3})),
+                Arguments.arguments("2 + 3x + 4x^2 + 5x^3", new Polynomial(new int[] {2, 3, 4, 5})),
+                Arguments.arguments("", new Polynomial(new int[] {}))
+        );
     }
 }
