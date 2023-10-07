d09af29eb97ef8fa7c88bf62ae52c5bacee59ef6
fojnk <fojnk@users.noreply.github.com>
2023-10-07 12:06:01 +0000

    Deploying to gh-pages from @ fojnk/OOP@aa567ee497da3705e01caba2d2caaa5e802750ed 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"BFSIterator"},{"p":"org.example","l":"DFSIterator"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Tree"}]
