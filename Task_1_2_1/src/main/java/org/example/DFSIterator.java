b7da465e94eeccf1f1bee28510b4065a75c22f71
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-10-14 00:43:46 +0700

    codestyle

--- Task_1_2_1/src/main/java/org/example/DFSIterator.java
+++ Task_1_2_1/src/main/java/org/example/DFSIterator.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.awt.*;
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.Stack;
--- Task_1_2_1/src/test/java/org.example/DFSIteratorTest.java
+++ Task_1_2_1/src/test/java/org.example/DFSIteratorTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.util.stream.Stream;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
