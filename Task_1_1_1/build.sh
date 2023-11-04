5d3ee6a073784706bb7fa43ee869999cc645b83e
Vova Petrov <109230619+fojnk@users.noreply.github.com>
2023-11-04 18:53:26 +0700

    clean up branch

--- Task_1_1_1/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-javac ./src/main/java/org/example/Main.java ./src/main/java/org/example/Heapsort.java -d ./mybuild/bin/
-javadoc -d ./mybuild/javadoc/ ./src/main/java/org/example/Main.java ./src/main/java/org/example/Heapsort.java
-java -classpath ./mybuild/bin/ org.example.Main
