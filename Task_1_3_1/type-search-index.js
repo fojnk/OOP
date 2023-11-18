04baca47b70c51aa55787f401b786aa37e7eed46
fojnk <fojnk@users.noreply.github.com>
2023-11-18 16:59:33 +0000

    Deploying to gh-pages from @ fojnk/OOP@4373e79c662c627519019f2a287b87de5c2a6ee5 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"ApostolicoCrochemoreAlgorithm"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"ApostolicoCrochemoreAlgorithm"},{"p":"org.example","l":"Main"}]
