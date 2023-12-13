c7542b435af53851b926851f2d6db53b897efad2
Vova Petrov <v.petrov3@g.nsu.ru>
2023-12-13 21:35:37 +0700

    work?

--- Task_1_5_2/src/test/java/org/example/UserInterfaceTest.java
+++ Task_1_5_2/src/test/java/org/example/UserInterfaceTest.java
@@ -1,10 +1,38 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 public class UserInterfaceTest {
     @Test
-    public void basicTests() {
+    public void addTest() throws IOException {
+        var ui = new UserInterface("notebook.json");
+        ui.ExecCommand(new String[] {"-add", "my title", "hshahfdlasj"});
 
+        File file = new File("notebook.json");
+        var notes = Json.readNotes(file);
+        Assertions.assertEquals("my title", notes.get(notes.size() - 1).getTitle());
+        Assertions.assertEquals("hshahfdlasj", notes.get(notes.size() - 1).getDescription());
+    }
+
+    @Test
+    public void removeTest() throws IOException {
+        var ui = new UserInterface("notebook.json");
+        ui.ExecCommand(new String[] {"-rm", "my title"});
+
+        File file = new File("notebook.json");
+        var notes = Json.readNotes(file);
+
+        var flag = true;
+        for (var note: notes) {
+            if (note.getTitle().equals("my title")) {
+                flag = false;
+                break;
+            }
+        }
+        Assertions.assertTrue(flag);
     }
 }
