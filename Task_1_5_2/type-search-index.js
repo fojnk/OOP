1fe999c5ad5a967a67dadcec2632f455e6805902
fojnk <fojnk@users.noreply.github.com>
2023-12-14 15:55:29 +0000

    Deploying to gh-pages from @ fojnk/OOP@e51c4de0ee7067d16a1561c6b42fcd69633d23a0 🚀

--- Task_1_5_2/type-search-index.js
+++ Task_1_5_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Json"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Note"},{"p":"org.example","l":"Notebook"},{"p":"org.example","l":"UserInterface"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"CommandLineParser"},{"p":"org.example","l":"Json"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Note"},{"p":"org.example","l":"Notebook"},{"p":"org.example","l":"NotebookApp"}]
