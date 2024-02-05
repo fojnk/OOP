64abb69396b8792bfb002403082bb57987552330
Vova Petrov <v.petrov3@g.nsu.ru>
2024-02-05 15:53:27 +0700

    checkstyle

--- Task_2_1_1/src/test/java/org/example/PrimeNumbersCheckerTest.java
+++ Task_2_1_1/src/test/java/org/example/PrimeNumbersCheckerTest.java
@@ -8,10 +8,18 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * Класс для тестов.
+ */
 public class PrimeNumbersCheckerTest {
+    /**
+     * Проверка метода IsPrime во всех чекерах.
+     *
+     * @param checker - разные реализации чекеров
+     */
     @ParameterizedTest
     @MethodSource("generateCheckersForTests")
-    public void PrimeTest(PrimeNumbersChecker checker) {
+    public void primeTest(PrimeNumbersChecker checker) {
         var list1 = Arrays.asList(1, 3, 5, 7, 2, 2, 2, 11, 13, 2, 1);
         for (int i = 0; i < list1.size(); i++) {
             Assertions.assertTrue(checker.IsPrime(list1.get(i)));
@@ -23,9 +31,15 @@ public class PrimeNumbersCheckerTest {
         }
     }
 
+    /**
+     * Проверка метода checkList во всех чекерах.
+     *
+     * @param checker - разные реализации чекеров
+     * @throws InterruptedException - ошибка в одном из потоков
+     */
     @ParameterizedTest
     @MethodSource("generateCheckersForTests")
-    public void CheckListTest(PrimeNumbersChecker checker) throws InterruptedException {
+    public void checkListTest(PrimeNumbersChecker checker) throws InterruptedException {
         var list1 = Arrays.asList(1, 3, 5, 7, 2, 2, 2, 11, 13, 2, 1);
         Assertions.assertTrue(checker.checkList(list1));
 
@@ -33,9 +47,16 @@ public class PrimeNumbersCheckerTest {
         Assertions.assertFalse(checker.checkList(list2));
     }
 
+    /**
+     * Проверка многопоточной реализации.
+     *
+     * @param list     - список чисел
+     * @param expected - ожидаемое значение
+     * @throws InterruptedException - ошибка в одном из потоков
+     */
     @ParameterizedTest
     @MethodSource("generateListsForTests")
-    public void ThreadsTest(List<Integer> list, boolean expected) throws InterruptedException {
+    public void threadsTest(List<Integer> list, boolean expected) throws InterruptedException {
         var checker1 = new MultiThreadPrimeNumbersSearcher();
         for (int i = 1; i < list.size(); i++) {
             checker1.setAmountOfThreads(i);
@@ -43,6 +64,11 @@ public class PrimeNumbersCheckerTest {
         }
     }
 
+    /**
+     * генерация чекеров.
+     *
+     * @return - чекеры
+     */
     static Stream<Arguments> generateCheckersForTests() {
         var checker1 = new MultiThreadPrimeNumbersSearcher();
         var checker2 = new SequentialPrimeNumbersSearcher();
@@ -54,6 +80,11 @@ public class PrimeNumbersCheckerTest {
         );
     }
 
+    /**
+     * генерация списков и ожидаемых значений.
+     *
+     * @return - списки и ожидаемые значения
+     */
     static Stream<Arguments> generateListsForTests() {
         var list1 = Arrays.asList(1, 3, 5, 7, 2, 2, 2, 11, 13, 2, 1);
         var list2 = Arrays.asList(18, 25, 9, 4, 6, 6, 8, 10, 12, 22, 32, 14);
