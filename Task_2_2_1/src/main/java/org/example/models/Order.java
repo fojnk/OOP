9ff7561d390aa087a4241864adba18f805a400f2
Vova Petrov <v.petrov3@g.nsu.ru>
2024-03-05 01:27:47 +0700

    fix build

--- Task_2_2_1/src/main/java/org/example/models/Order.java
+++ Task_2_2_1/src/main/java/org/example/models/Order.java
@@ -26,14 +26,30 @@ public class Order {
         this.status = newStatus;
         String statusMessage = null;
         switch (this.status) {
-            case IN_QUEUE -> statusMessage = "in queue for cooking";
-            case ACCEPTED -> statusMessage = "accepted";
-            case IS_COOKING -> statusMessage = "is cooking";
-            case WAITING_TO_BE_SENT_TO_STORAGE -> statusMessage = "waiting to be sent to storage";
-            case IN_STORAGE -> statusMessage = "in storage";
-            case DELIVERING -> statusMessage = "delivering";
-            case DONE -> statusMessage = "done";
-            default -> statusMessage = "unknown status";
+            case IN_QUEUE:
+                statusMessage = "in queue for cooking";
+                break;
+            case ACCEPTED:
+                statusMessage = "accepted";
+                break;
+            case IS_COOKING:
+                statusMessage = "is cooking";
+                break;
+            case WAITING_TO_BE_SENT_TO_STORAGE:
+                statusMessage = "waiting to be sent to storage";
+                break;
+            case IN_STORAGE:
+                statusMessage = "in storage";
+                break;
+            case DELIVERING:
+                statusMessage = "delivering";
+                break;
+            case DONE:
+                statusMessage = "done";
+                break;
+            default:
+                statusMessage = "unknown status";
+                break;
         }
         return String.format("[%d] [%s]", this.id, statusMessage);
     }
