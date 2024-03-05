495854f2697555b269ce1dcfb87a53fea35b7c9c
Vova Petrov <v.petrov3@g.nsu.ru>
2024-03-06 00:07:09 +0700

    Pizzeria test?

---
+++ Task_2_2_1/src/test/java/org/example/PizzeriaTest.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class PizzeriaTest {
+    @Test
+    public void checkWorkTest() {
+        Main.main(new String[]{});
+    }
+}
